8ba0ec5e0c59ca4cce73080304c9735477f69a0c
Makish2023 <saginikevin96@gmail.com>
2023-02-16 14:12:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- * main -is the main entry of the program
+ * main -is the main function of the program
  *
  * Return:always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
